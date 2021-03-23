f91a0bed91b6a163f65e32a5d89880666abb09c4
Robert Novotny <novotnr0@gmail.com>
2021-03-23 18:14:31 +0100

    Pripraveny view binding a nastavenie layoutu pre RecyclerView

--- app/src/main/java/com/github/novotnyr/yello/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/yello/MainActivity.kt
@@ -3,11 +3,20 @@ package com.github.novotnyr.yello
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import com.github.novotnyr.yello.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding
+            .inflate(layoutInflater)
+            .apply { setContentView(root) }
+
+        binding.noteRecyclerView.layoutManager = GridLayoutManager(this, 3)
     }
 
     fun onFloatingActionButtonClick(view: View) {}
