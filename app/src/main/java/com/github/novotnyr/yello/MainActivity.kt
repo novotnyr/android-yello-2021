1168e8d67f32cba56213b8c12a9a05b2f43c78bf
Robert Novotny <novotnr0@gmail.com>
2021-03-23 17:01:48 +0100

    Pridany layout s RecyclerView, Floating Action Button a obsluhou kliku

--- app/src/main/java/com/github/novotnyr/yello/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/yello/MainActivity.kt
@@ -2,10 +2,13 @@ package com.github.novotnyr.yello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onFloatingActionButtonClick(view: View) {}
 }
