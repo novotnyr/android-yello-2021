6323bd810dc52a29f53f20308f8966c75050aff3
Robert Novotny <novotnr0@gmail.com>
2021-03-24 02:20:42 +0100

    Pridany viewholder

--- app/src/main/java/com/github/novotnyr/yello/NoteListAdapter.kt
+++ app/src/main/java/com/github/novotnyr/yello/NoteListAdapter.kt
@@ -1,6 +1,10 @@
 package com.github.novotnyr.yello
 
+import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 object NoteDiff : DiffUtil.ItemCallback<Note>() {
     override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
@@ -10,5 +14,13 @@ object NoteDiff : DiffUtil.ItemCallback<Note>() {
     override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
         return oldItem.description == newItem.description
                 && oldItem.timestamp == newItem.timestamp
+    }
+}
+
+class NoteViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+    val text1: TextView = root.findViewById(android.R.id.text1)
+
+    fun bind(note: Note) {
+        text1.text = "${note.description} (${Date(note.timestamp)})"
     }
 }
